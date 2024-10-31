4b14c68c2940656014b9d3ad1ce3aefeb790d55b
Chali Mohamedsani <chalisani@hotmail.com>
2024-10-31 11:00:55 +0100

    Initial Commit

---
+++ bokamatch/bokamatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  bokamatchApp.swift
+//  bokamatch
+//
+//  Created by Chali Mohamedsani on 2024-10-31.
+//
+
+import SwiftUI
+
+@main
+struct bokamatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bokamatchTests/bokamatchTests.swift
@@ -0,0 +1,16 @@
+//
+//  bokamatchTests.swift
+//  bokamatchTests
+//
+//  Created by Chali Mohamedsani on 2024-10-31.
+//
+
+import Testing
+
+struct bokamatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
